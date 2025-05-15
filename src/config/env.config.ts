1cb836449ef45c8ce778ba40724d1b53b7f7f85f
Uttreshwar Nakhate <you@example.com>
2025-05-15 15:30:47 +0530

    feat: Environment setup done

---
+++ src/config/env.config.ts
@@ -0,0 +1,27 @@
+import DotenvFlow from 'dotenv-flow'
+DotenvFlow.config()
+
+// console.log(process.env)
+
+export default {
+    // The port on which the server will run
+    APP_PORT: process.env.APP_PORT,
+
+    // The URL of the database
+    DB_URL: process.env.DB_URL,
+
+    // The secret key for JWT
+    JWT_SECRET: process.env.JWT_SECRET,
+
+    // The URL of the frontend application
+    FRONTEND_URL: process.env.FRONTEND_URL,
+
+    // The URL of the backend application
+    BACKEND_URL: process.env.BACKEND_URL,
+
+    //JWT access token secret
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+
+    // JWT refresh token secret
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET
+}
